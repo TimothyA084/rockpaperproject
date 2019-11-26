2cd141826c0a664ea98c6ad4fb8347a3da7bdbbc
TimothyA084 <taaj0929@gmail.com>
2019-11-25 22:23:27 -0500

    project setup base html page with empty functions js

---
+++ rps.js
@@ -0,0 +1,23 @@
+console.log("test the script is active")
+
+// functions 
+
+function computerPlay(){
+
+}
+
+function computerSelection(){
+
+}
+
+function playerSelection(){
+
+}
+
+function playRound(){
+
+}
+
+function game(){
+
+}
