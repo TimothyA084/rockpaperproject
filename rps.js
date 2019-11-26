33fe56251a6e1bcef7ad8601c68b1407e4e03cd2
TimothyA084 <taaj0929@gmail.com>
2019-11-25 22:59:01 -0500

    began working on functions for the game

--- rps.js
+++ rps.js
@@ -8,14 +8,29 @@ function computerPlay(){
 
 function computerSelection(){
 
+    var cplay=(math.random() * 3)
+
+    if (cplay == 1)
+    return "rock"
+    
+    else if (cplay == 2)
+    return "paper"
+    
+    else (cplay == 3)
+    return "scissors"
+
 }
 
 function playerSelection(){
 
+    var pchoice = prompt("Choose rock, paper, or scissors.")
 }
 
 function playRound(){
 
+    if (playerSelection == computerSelection)
+    return "it is a draw"
+    
 }
 
 function game(){
